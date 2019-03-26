77fd87dd6280b7228623e3d75fea0ae99c9cfa75
Lowry West <4lowrywest@gmail.com>
2019-03-25 20:09:57 -0500

    Inital work

--- BoardGame.java
+++ BoardGame.java
@@ -2,12 +2,41 @@ import java.util.LinkedHashMap;
 
 public class BoardGame 
 {
+	//Initalizing the LinkedHashMaps
 	protected  LinkedHashMap<String, Location> locMap;
 	protected  LinkedHashMap<String, GamePiece> gameMap;
+	//Creating a private variable to hold the location to add the Player
+	private int loc=0;
 	
 	public BoardGame()
 	{
 		locMap=new LinkedHashMap<String,Location>();
 		gameMap=new LinkedHashMap<String,GamePiece>();  
+	}
+	
+	public boolean addPlayer(String playerName, GamePiece gamePiece,  Location intialLocation)
+	{
+		if(locMap.containsKey(playerName)||gameMap.containsKey(playerName))
+		{
+			return false;
+		}
+		else
+		{
+			locMap.put(playerName, intialLocation);
+			gameMap.put(playerName, gamePiece);
+			return true;
+		}
+		
+	}
+	
+	public GamePiece getPlayerGamePiece(String playerName)
+	{
+		return gameMap.get(playerName);
+	}
+	
+	public String getPlayerWithGamePiece(GamePiece gamePiece)
+	{
+		return gameMap.get(gamePiece);
+		
 	}
 }
