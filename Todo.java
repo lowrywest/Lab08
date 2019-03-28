fe0f61742335ac353e6459632b1d0f6ffca9e554
lowrywest <47228110+lowrywest@users.noreply.github.com>
2019-03-28 12:06:57 -0500

    Create Todo.java

---
+++ Todo.java
@@ -0,0 +1,33 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-3-26
+OU ID_______________: 113410037
+OU EMAIL____________: lowry.y.west-1@ou.edu
+
+Tasks:
+1. Color Enum & Tests 
+2. Shape Enum & Tests
+3. Location Enum & Tests
+4. GamePiece Class & Tests
+5. GamePieceAppearance Enum & Tests
+6. BoardGame Class & Tests
+7. Driver Class
+
+Estimated Time (minutes):
+1. 15
+2. 15
+3. 25
+4. 30
+5. 15
+6. 50
+7. 20
+
+Actual Time (minutes):
+1. 10
+2. 15
+3. 30
+4. 45
+5. 25
+6. 100
+7. 15
+*/}
